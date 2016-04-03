3a43c2df7b3041cb538f937180f208ef74f72662
ishotjr <github@ishotjr.com>
2016-04-03 15:10:37 -0400

    :fire: :trophy: Add steps leader PoC

--- src/js/app.js
+++ src/js/app.js
@@ -10,6 +10,34 @@ function firebaseCall(username, steps) {
 	myDataRef.push({name: username, text: steps});
 }
 
+function firebaseRead() {
+
+/*
+	var ref = new Firebase("https://nk6bl51k7ig.firebaseio-demo.com");
+	// Attach an asynchronous callback to read the data
+	ref.on("value", function(snapshot) {
+	  console.log('snapshot.val(): ' + JSON.stringify(snapshot.val()));
+	}, function (errorObject) {
+	  console.log("The read failed: " + errorObject.code);
+	});
+*/
+
+	// TODO: FIGURE OUT HOW TO NOT MAKE THIS SUPER INEFFICIENT!!
+	var leaderName = "";
+	var leaderSteps = 0;
+
+	var ref = new Firebase("https://nk6bl51k7ig.firebaseio-demo.com");
+	ref.orderByChild("text").on("child_added", function(snapshot) {
+	  //console.log("name: " + snapshot.key() + " steps: " + snapshot.val().text);
+	  leaderName = snapshot.val().name;
+	  leaderSteps = snapshot.val().text;
+
+	  console.log("leaderName: " + leaderName + " leaderSteps: " + leaderSteps);
+	});
+
+	// TODO: pass results
+}
+
 // Listen for when the watchface is opened
 Pebble.addEventListener('ready', 
 	function(e) {
@@ -25,6 +53,7 @@ Pebble.addEventListener('appmessage',
 		var dict = e.payload;
 		console.log('Got message: ' + JSON.stringify(dict));
 
-		firebaseCall(e.payload.username, e.payload.steps);
+		//firebaseCall(e.payload.username, e.payload.steps);
+		firebaseRead();
 	}                     
 );
